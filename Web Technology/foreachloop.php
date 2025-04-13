5171a0c65151122a92f6a7b0efe5610a9718dcd8
Nishchal Acharya <140315374+Nishchal-ll@users.noreply.github.com>
2025-04-13 20:13:07 +0545

    Create foreachloop.php

---
+++ Web Technology/foreachloop.php
@@ -0,0 +1,8 @@
+<?php
+$numbers = [1, 2, 3, 4, 5]; 
+echo "Original numbers and their squares:<br>";
+foreach ($numbers as $num) {
+    $square = $num * $num;
+    echo "Number: $num, Square: $square<br>";
+}
+?>
